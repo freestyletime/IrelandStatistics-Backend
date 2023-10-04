3aeb485f341223a76bef2fdb3bc848731c84e656
Chris Chen <mailchristianchen@gmail.com>
2023-10-04 15:23:48 +0100

    init project

---
+++ src/main/java/ie/chrischen/irelandstatistics/IrelandStatisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package ie.chrischen.irelandstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrelandStatisticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrelandStatisticsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/chrischen/irelandstatistics/IrelandStatisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.chrischen.irelandstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrelandStatisticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
